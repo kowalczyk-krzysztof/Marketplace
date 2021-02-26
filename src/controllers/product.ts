a6227453d0f4e54afbfcb28aede2d7aa99602f5e
Krzysztof Kowalczyk <kowalczykkrzysztof893@gmail.com>
2021-02-26 21:34:05 +0100

    Added express-async-handler and edited error middleware

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,58 +1,50 @@
 import { RequestHandler } from 'express';
 import ProductSchema from '../models/Product';
+import { ErrorResponse } from '../utils/errorResponse';
+import asyncHandler from 'express-async-handler';
 
 // @desc    Get all products
 // @route   GET /api/v1/products
 // @access  Public
-export const getProducts: RequestHandler = async (req, res, next) => {
-  try {
-    const products = await ProductSchema.find();
+export const getProducts: RequestHandler = asyncHandler(async (req, res) => {
+  const products = await ProductSchema.find();
 
-    res.status(200).json({
-      success: true,
-      numberOfProducts: products.length,
-      data: products,
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(200).json({
+    success: true,
+    numberOfProducts: products.length,
+    data: products,
+  });
+});
 // @desc    Get single product
 // @route   GET /api/v1/products/:id
 // @access  Public
-export const getProduct: RequestHandler = async (req, res, next) => {
-  try {
+export const getProduct: RequestHandler = asyncHandler(
+  async (req, res, next) => {
     const product = await ProductSchema.findById(req.params.id);
 
     // If ID format is valid but it doesn't exist
     if (!product) {
-      return res.status(400).json({ success: false, msg: 'ID does not exist' });
+      return next(
+        new ErrorResponse(`Product not found with id of ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ sucess: true, data: product });
-  } catch (err) {
-    // If ID is in invalid format
-    // res.status(400).json({ success: false, msg: 'Invalid ID' });
-    next(err);
   }
-};
+);
 // @desc    Create product
 // @route   PUT /api/v1/products/
 // @access  Private
-export const createProduct: RequestHandler = async (req, res, next) => {
-  try {
-    const product = await ProductSchema.create(req.body);
-    res.status(201).json({ success: true, data: product });
-  } catch (err) {
-    res.status(400).json({ success: false, msg: 'Duplicate entry' });
-  }
-};
+export const createProduct: RequestHandler = asyncHandler(async (req, res) => {
+  const product = await ProductSchema.create(req.body);
+  res.status(201).json({ success: true, data: product });
+});
 
 // @desc    Update product
 // @route   PUT /api/v1/products/:id
 // @access  Private
-export const updateProduct: RequestHandler = async (req, res, next) => {
-  try {
+export const updateProduct: RequestHandler = asyncHandler(
+  async (req, res, next) => {
     const product = await ProductSchema.findByIdAndUpdate(
       req.params.id,
       req.body,
@@ -62,27 +54,25 @@ export const updateProduct: RequestHandler = async (req, res, next) => {
       }
     );
     if (!product) {
-      return res.status(400).json({ success: false, msg: 'ID does not exist' });
+      return next(
+        new ErrorResponse(`Product not found with id of ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ sucess: true, data: product });
-  } catch (err) {
-    res.status(400).json({ success: false, msg: 'Something went wrong' });
   }
-};
+);
 
 // @desc    Delete product
 // @route   DELETE /api/v1/products/:id
 // @access  Private
-export const deleteProduct: RequestHandler = async (req, res, next) => {
-  try {
-    const product = await ProductSchema.findByIdAndDelete(req.params.id);
-    if (!product) {
-      return res.status(400).json({ success: false, msg: 'ID does not exist' });
-    }
-
-    res.status(200).json({ sucess: true, data: {} });
-  } catch (err) {
-    res.status(400).json({ success: false, msg: 'Something went wrong' });
+export const deleteProduct = asyncHandler(async (req, res, next) => {
+  const product = await ProductSchema.findByIdAndDelete(req.params.id);
+  if (!product) {
+    return next(
+      new ErrorResponse(`Product not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ sucess: true, data: {} });
+});
--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,12 +1,37 @@
+import { KeyObject } from 'crypto';
 import { ErrorRequestHandler } from 'express';
+import { ErrorResponse } from '../utils/errorResponse';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
   // Log to console for dev
-  console.log(err.stack);
+  console.log(err);
+  // Mongoose bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors)
+      .map((val: any) => val.message)
+      .toString()
+      .split(',')
+      .join(' | ');
+    // TODO1: Figure out the type here
+    // TODO2: Find a better way of handling this. Currently it is done like that because errorResponse has to return a string
+    error = new ErrorResponse(message, 400);
+  }
 
-  res.status(500).json({
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message,
+    error: error.message || 'Server Error',
   });
 };
 
